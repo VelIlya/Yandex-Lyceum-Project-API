ef6598bb1c536e074f1994f652c0e703f5e68442
Ilya <VelIlyafms@yandex.ru>
2024-04-18 22:35:46 +0500

    Upd1.01

--- main.py
+++ main.py
@@ -1,36 +1,20 @@
 from telegram.ext import Application, MessageHandler, filters, CommandHandler
-import requests
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 
 
-async def start(update, context):
-    """Отправляет сообщение когда получена команда /start"""
-    user = update.effective_user
-    await update.message.reply_html(
-        rf"Привет {user.mention_html()}! Я бот-помошник для путешествий...",
-    )
-
-
-async def help_command(update, context):
-    """Отправляет сообщение когда получена команда /help"""
-    await update.message.reply_text("Я пока не умею помогать...")
+async def notification(update, context):
+    await update.message.reply_text("Напоминания")
 
 
 def main():
-    # Запуск бота.
     application = Application.builder().token(BOT_TOKEN).build()
-
-    # Регистрация комманд.
-    application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("help", help_command))
-
-    # Запускаем приложение.
+    application.add_handler(CommandHandler("notification", notification))
     application.run_polling()
 
 
-# Запускаем функцию main() в случае запуска скрипта.
 if __name__ == '__main__':
     main()
